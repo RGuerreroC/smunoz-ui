6ecb01157f09ee0aadc6e1acc46a3f0170add75d
Sandra Muñoz <smunoz@inbenta.com>
2018-12-02 18:36:57 +0100

    New react main structure

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import './App.scss';
+import './styles/App.scss';
 
 import Header from './components/header/Header'
 import Navigation from './components/navigation/Navigation'
-
+import Section from './components/section/Section';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
         <Navigation />
-        
+        <Section />
       </div>
     );
   }
---
+++ src/components/section/Section.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Section extends Component {
+  render() {
+    return (
+      <section className="about-me">
+        <h2>About me</h2>
+      </section>
+  );
+  }
+}
+
+export default Section;
